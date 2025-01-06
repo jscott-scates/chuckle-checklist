b1c4ec5305249e898933e7e9bf5877e60103f59b
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2025-01-06 17:13:39 -0500

    working on Add Joke Section

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css"
+
+export const App = () => {
+  return <>
+    <div className="app-container">
+      <article className="app-heading">
+       <h1 className="app-heading-text">Chuckle Checklist</h1>
+      </article>
+
+      <div>
+        <h2>Add Joke</h2>
+      </div>
+      
+
+    </div>
+  </>
+}
